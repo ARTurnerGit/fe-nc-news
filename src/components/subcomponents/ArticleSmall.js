977b4e9f7e09b84a2449e5b12d16fe614dd4b6e7
Alun Turner <alun.turner@googlemail.com>
2020-05-01 19:32:47 +0100

    working on ArticleLarge

--- src/components/subcomponents/ArticleSmall.js
+++ src/components/subcomponents/ArticleSmall.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 class ArticleSmall extends React.Component {
   state = {};
@@ -6,8 +7,12 @@ class ArticleSmall extends React.Component {
     return (
       <div>
         <p>
-          Hi, I'm a small article about {this.props.topic}, created on{" "}
-          {this.props.created_at} with {this.props.votes} votes and{" "}
+          <Link to={`/${this.props.topic}/${this.props.article_id}`}>
+            Hi, I'm a small article
+          </Link>
+          {" about "}
+          <Link to={`/${this.props.topic}`}>{this.props.topic}</Link>, created
+          on {this.props.created_at} with {this.props.votes} votes and{" "}
           {this.props.comment_count} comments
         </p>
       </div>

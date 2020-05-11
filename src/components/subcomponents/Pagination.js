abb1c5c5e00177c8fdc6117f6a3f7ab2e187ff10
Alun Turner <alun.turner@googlemail.com>
2020-05-11 14:00:12 +0100

    complete laptop implementation pagination

--- src/components/subcomponents/Pagination.js
+++ src/components/subcomponents/Pagination.js
@@ -3,21 +3,23 @@ import React from "react";
 function Pagination({ p, total_p, updatePage, updateLimit }) {
   return (
     <>
-      <button onClick={() => updatePage(-1)} disabled={p === 1}>
-        Previous
-      </button>
-      {`${p} / ${total_p}`}
-      <button onClick={() => updatePage(1)} disabled={p === total_p}>
-        Next
-      </button>
-      <span>SHOW ME</span>
-
-      <button onClick={() => updateLimit(1)}>1</button>
-      <button onClick={() => updateLimit(2)}>2</button>
-      <button onClick={() => updateLimit(5)}>5</button>
-      <button onClick={() => updateLimit(10)}>10</button>
-      <button onClick={() => updateLimit(100)}>ALL</button>
-      <span>RESULTS PER PAGE</span>
+      <div>
+        <button onClick={() => updatePage(-1)} disabled={p === 1}>
+          Previous
+        </button>
+        {`${p} / ${total_p}`}
+        <button onClick={() => updatePage(1)} disabled={p === total_p}>
+          Next
+        </button>
+      </div>
+      <div>
+        showing
+        <button onClick={() => updateLimit(1)}>1</button>
+        <button onClick={() => updateLimit(2)}>2</button>
+        <button onClick={() => updateLimit(5)}>5</button>
+        <button onClick={() => updateLimit(10)}>10</button>
+        <button onClick={() => updateLimit(100)}>ALL</button>
+      </div>
     </>
   );
 }
